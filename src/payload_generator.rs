ee74d6eb249f5ce2f64d161ca351754a696d2d92
rigma <rigbuntu@gmail.com>
2020-02-16 22:14:20 +0100

    refactor: change return type from generate method
    fix: add missing documentation

--- src/payload_generator.rs
+++ src/payload_generator.rs
@@ -2,5 +2,9 @@
 /// generator structure shpuld implement in order to slice
 /// data into suitable RTP packet's payloads.
 pub trait PayloadGenerator {
-    fn generate(&self, maximum_transmission_unit: usize, payload: &[u8]) -> Vec<Vec<u8>>;
+    /// Try to generates a sequence of suitable RTP packet's payload
+    /// from MTU (Maximum Transmission Unit) and an arbitrary payload.
+    ///
+    /// If no payloads can be generated, this method should return `None`.
+    fn generate(&self, mtu: usize, payload: &[u8]) -> Option<Vec<Vec<u8>>>;
 }
