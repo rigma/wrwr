36b6b172eee64ba7fee26b512be2d5187d9f963a
rigma <rigbuntu@gmail.com>
2020-05-12 20:09:29 +0200

    feat: add custom ser/de error enum

--- serde/src/error.rs
+++ serde/src/error.rs
@@ -1,1 +1,30 @@
+use serde::{de, ser};
+use std::{fmt, io};
 
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("An error was emitted from the reader/writer used during serialization/deserialization process!")]
+    Io(#[from] io::Error),
+    #[error("A custom error was emitted from Serde. Message: {0}")]
+    Custom(String),
+}
+
+impl de::Error for Error {
+    fn custom<D>(msg: D) -> Self
+    where
+        D: fmt::Display,
+    {
+        Self::Custom(msg.to_string())
+    }
+}
+
+impl ser::Error for Error {
+    fn custom<D>(msg: D) -> Self
+    where
+        D: fmt::Display,
+    {
+        Self::Custom(msg.to_string())
+    }
+}
